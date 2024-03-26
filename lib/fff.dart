09efda48857d8241c383eea307e6e3e5d39040cb
Hend <hendgamil@gmail.com>
2024-03-26 23:10:02 +0200

    jjjj

---
+++ lib/fff.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text('hhhhhhh'),
+              Text('hhhhhhh'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('hhhhhhh'),
+              Text('hhhhhhh'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
